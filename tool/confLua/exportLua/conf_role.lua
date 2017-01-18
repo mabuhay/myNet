0cd2936ee4e0506505b4f33507b9d07310ff6ab7
mabuhay <271275944@qq.com>
2017-01-18 15:59:09 +0800

    sport

--- tool/confLua/exportLua/conf_role.lua
+++
@@ -1,4 +0,0 @@
--- head: id | whattype | job | name | skills | gID | brect | complex_struct
-conf_role = {}
-conf_role[1000]={name='是野兽战士吗', skills={[1]=1000, [2]=1001, }, brect={y=0, x=-50, w=100, h=150, }, job='战士', gID=1000, complex_struct={a={[1]=1, [2]=2, [3]=3, }, acb={kji=2, }, }, whattype='肉搏类', }
-conf_role[2000]={name='是野兽法师吗', skills={[1]=1000, [2]=1001, }, brect={y=0, x=-50, w=100, h=150, }, job='法师', gID=2000, complex_struct={a={[1]=1, [2]=2, [3]=3, }, acb={kji=2, }, }, whattype='控制类', }
